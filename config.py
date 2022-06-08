0d7a5aa84221e19b60414be155d0c42eb5fe8051
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-06-08 23:52:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 BOT_TOKEN = 5591294501:AAHzgO2RGiHCvmuVQvOSfMygeVJzlF_qF3U
-API_ID = os.environ.get("19258466")
+API_ID = os.environ.get("5591294501")
 API_HASH = os.environ.get("ee016b97a9215ae5544e2ce467d2dad7")
 
-
+5591294501
 # DATOS DEL MOODLE
 USUARIO = os.environ.get("emsosa")
 PASSWORD = os.environ.get("ernesT-o09")
