d16b66797f15b15bca4579885908eb8a4d795ae8
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-06-08 16:49:27 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+
+BOT_TOKEN = 5214141627:AAEbh0W6Q07Qz6OMD-o_CrJM2oAgFVCaDvY
+API_ID = os.environ.get("9902519")
+API_HASH = os.environ.get("9d8097d05bbc90a6ed2a7a81abcd4e8a")
+
+
+# DATOS DEL MOODLE
+USUARIO = os.environ.get("agmorejon@uclv.cu")
+PASSWORD = os.environ.get("Lachel*9145")
+USUARIO_ID = os.environ.get("6246")
+ZIP_MB = os.environ.get("200")
+MOODLE_URL = os.environ.get("moodle.uclv.edu.cu")
+OWNER = os.environ.get("1161231954")
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'yanco148@gmail.com'
+PASS_MEGA = 'yancarlos1482005'
