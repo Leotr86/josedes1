b46c82c21974901e0af2649a66ff8c75c3febba3
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-06-08 23:29:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
 
-BOT_TOKEN = 5214141627:AAEbh0W6Q07Qz6OMD-o_CrJM2oAgFVCaDvY
-API_ID = os.environ.get("9902519")
-API_HASH = os.environ.get("9d8097d05bbc90a6ed2a7a81abcd4e8a")
+BOT_TOKEN = 5591294501:AAHzgO2RGiHCvmuVQvOSfMygeVJzlF_qF3U
+API_ID = os.environ.get("19258466")
+API_HASH = os.environ.get("ee016b97a9215ae5544e2ce467d2dad7")
 
 
 # DATOS DEL MOODLE
-USUARIO = os.environ.get("agmorejon@uclv.cu")
-PASSWORD = os.environ.get("Lachel*9145")
+USUARIO = os.environ.get("emsosa")
+PASSWORD = os.environ.get("ernesT-o09")
 USUARIO_ID = os.environ.get("6246")
 ZIP_MB = os.environ.get("200")
 MOODLE_URL = os.environ.get("moodle.uclv.edu.cu")
-OWNER = os.environ.get("1161231954")
+OWNER = os.environ.get("618303071")
 
 # CUENTA DE MEGA
 GMAIL_MEGA = 'yanco148@gmail.com'
